b5909a8b393d5aadd49c123e4414696cc7664ff2
Adam Smith <adam@roadlyfe.com>
2023-02-27 13:40:23 -0800

    created and implemented preview mode functionality in page.tsx(user)

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { previewData } from "next/headers";
 
 function homePage() {
-  return (
-    <div></div>
-  )
-}
+  
+  if(previewData()) {
+    return (
+      <div>Preview Mode</div>
+    )
+    }
+     
+    return (
+      <div>
+        <h1>Not in Preview Mode</h1>
+      </div>
+    );
+  }
+      
 
 export default homePage 
