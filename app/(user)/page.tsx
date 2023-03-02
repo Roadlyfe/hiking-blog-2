9fbccf340e5aa5b0d7e2f3106df05523668f5d09
Adam Smith <adam@roadlyfe.com>
2023-03-02 13:12:51 -0800

    created and implemented PreviewBlogList component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,6 +1,8 @@
 import { previewData } from "next/headers";
 import { groq } from "next-sanity";
 import { client } from "../../lib/sanity.client";
+import { PreviewSuspense } from "next-sanity/preview";
+import PreviewBlogList from "../../components/PreviewBlogList";
 
 const query = groq`
 *[_type=='post'] {
@@ -14,12 +16,19 @@ export default async function homePage() {
 
   if (previewData()) {
     return (
-      <div>Preview Mode</div>
+      <PreviewSuspense fallback={(
+        <div role="status">
+          <p className="text-center text-lg animate-pulse text-[#06d6a0]">Loading Preview Data...</p>
+        </div>
+      )}
+      >
+        <PreviewBlogList query={query}/>
+      </PreviewSuspense>
     )
   }
 
   const posts = await client.fetch(query);
-  
+
   return (
     <div>
       <h1>Not in Preview Mode</h1>
