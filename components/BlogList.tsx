884618fd5d73b9bf6653bbf1d261f7bc065b818b
Adam Smith <adam@roadlyfe.com>
2023-03-05 15:17:42 -0800

    added the type definition for blog description

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,6 +1,7 @@
 import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import urlFor from "../lib/urlFor";
+import ClientSideRoute from "./ClientSideRoute";
 
 type Props = {
     posts: Post[];
@@ -13,7 +14,8 @@ function BlogList({ posts }: Props) {
             <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
                 {/* Posts */}
                 {posts.map(post => (
-                    <div key={post._id} className="flex flex-col group cursor-pointer">
+                    <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+                    <div  className="flex flex-col group cursor-pointer">
                         <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
                             <Image
                                 className="object-cover object-left lg:object-center"
@@ -54,6 +56,7 @@ function BlogList({ posts }: Props) {
                             <ArrowUpRightIcon className="ml-2 h-4 w-4" />
                         </p>
                     </div>
+                    </ClientSideRoute>
                 ))}
             </div>
         </div>
